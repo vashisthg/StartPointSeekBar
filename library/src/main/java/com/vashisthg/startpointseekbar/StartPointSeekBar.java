be469304fdae85f1379b301522bea62339b7045b
Gaurav Vashisth <vashisthg@gmail.com>
2015-06-09 19:17:17 +0530

    refactoring, seekbar drawable and color from layout xml.

--- library/src/main/java/com/vashisthg/startpointseekbar/StartPointSeekBar.java
+++ library/src/main/java/com/vashisthg/startpointseekbar/StartPointSeekBar.java
@@ -1,77 +1,73 @@
 package com.vashisthg.startpointseekbar;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
-import android.widget.ImageView;
 
 import java.math.BigDecimal;
 
 /**
  * Created by vashisthg on 01/04/14.
  */
-public class StartPointSeekBar<T extends Number>  extends View
-{
-    private final Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+public class StartPointSeekBar extends View {
+    private static final Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private static final int DEFAULT_RANGE_COLOR = Color.argb(0xFF, 0x33, 0xB5, 0xE5);
+    private static final int DEFAULT_BACKGROUND_COLOR = Color.GRAY;
+    private static final float DEFAULT_MIN_VALUE = -100f;
+    private static final float DEFAULT_MAX_VALUE = +100f;
 
-    private final T absoluteMinValue, absoluteMaxValue;
-    private final double absoluteMinValuePrim, absoluteMaxValuePrim;
+
+    private double absoluteMinValue;
+    private double absoluteMaxValue;
     private final Bitmap thumbImage;
     private final Bitmap thumbPressedImage;
+    private final int defaultRangeColor;
+    private final int defaultBackgroundColor;
+
     private final float thumbWidth;
     private final float thumbHalfWidth;
     private final float thumbHalfHeight;
     private final float lineHeight;
     private final float padding;
-    private int mScaledTouchSlop;
-    private boolean mIsDragging;
-    private final NumberType numberType;
-
-
+    private int scaledTouchSlop;
+    private boolean isDragging;
     private boolean isThumbPressed;
     private double normalizedThumbValue = 0d;
-
     private boolean notifyWhileDragging = true;
+    private OnSeekBarChangeListener listener;
 
-    private OnSeekBarChangeListener<T> listener;
 
-    public final int SINGLE_COLOR = Color.argb(0xFF, 0x33, 0xB5, 0xE5);
-    public static final int BACKGROUND_COLOR = Color.GRAY;
 
     /**
      * Callback listener interface to notify about changed range values.
      *
-     * @author Stephan Tittel (stephan.tittel@kom.tu-darmstadt.de)
-     *
-     * @param <T>
-     * The Number type the Seekbar has been declared with.
      */
-    public interface OnSeekBarChangeListener<T> {
-        public void onOnSeekBarValueChange(StartPointSeekBar<?> bar,
-                                                T value);
+    public interface OnSeekBarChangeListener {
+        void onOnSeekBarValueChange(StartPointSeekBar bar,
+                                           double value);
     }
 
     /**
      * Registers given listener callback to notify about changed selected
      * values.
      *
-     * @param listener
-     *            The listener to notify about changed selected values.
+     * @param listener The listener to notify about changed selected values.
      */
     public void setOnSeekBarChangeListener(
-            OnSeekBarChangeListener<T> listener) {
+            OnSeekBarChangeListener listener) {
         this.listener = listener;
     }
-
-
 
     /**
      * An invalid pointer id.
@@ -87,19 +83,43 @@ public class StartPointSeekBar<T extends Number>  extends View
     private float mDownMotionX;
     private int mActivePointerId = INVALID_POINTER_ID;
 
-    public StartPointSeekBar(T absoluteMinValue, T absoluteMaxValue, Context context)
-    {
-        super(context);
-        thumbImage = BitmapFactory.decodeResource(getResources(),
-                R.drawable.seek_thumb_normal);
-        thumbPressedImage = BitmapFactory.decodeResource(getResources(),
-                R.drawable.seek_thumb_pressed);
+    public StartPointSeekBar(Context context) {
+        this(context, null);
+    }
 
-        this.absoluteMinValue = absoluteMinValue;
-        this.absoluteMaxValue = absoluteMaxValue;
-        absoluteMinValuePrim = absoluteMinValue.doubleValue();
-        absoluteMaxValuePrim = absoluteMaxValue.doubleValue();
-        numberType = NumberType.fromNumber(absoluteMinValue);
+    public StartPointSeekBar(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public StartPointSeekBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        // Attribute initialization
+        final TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.StartPointSeekBar,
+                defStyleAttr, 0);
+
+        Drawable thumbImageDrawable = a.getDrawable(R.styleable.StartPointSeekBar_thumbDrawable);
+        if (thumbImageDrawable == null) {
+            thumbImageDrawable = getResources().getDrawable(R.drawable.seek_thumb_normal);
+        }
+        this.thumbImage = ((BitmapDrawable) thumbImageDrawable).getBitmap();
+
+        Drawable thumbImagePressedDrawable = a.getDrawable(R.styleable.StartPointSeekBar_thumbPressedDrawable);
+        if (thumbImagePressedDrawable == null) {
+            thumbImagePressedDrawable = getResources().getDrawable(R.drawable.seek_thumb_pressed);
+        }
+        this.thumbPressedImage = ((BitmapDrawable) thumbImagePressedDrawable).getBitmap();
+
+        this.absoluteMinValue = a.getFloat(R.styleable.StartPointSeekBar_minValue, DEFAULT_MIN_VALUE);
+        this.absoluteMaxValue = a.getFloat(R.styleable.StartPointSeekBar_maxValue, DEFAULT_MAX_VALUE);
+
+        this.defaultBackgroundColor = a.getColor(R.styleable.StartPointSeekBar_defaultBackgroundColor,
+                DEFAULT_BACKGROUND_COLOR);
+        this.defaultRangeColor = a.getColor(R.styleable.StartPointSeekBar_defaultBackgroundRangeColor,
+                DEFAULT_RANGE_COLOR);
+
+        a.recycle();
+
         thumbWidth = thumbImage.getWidth();
         thumbHalfWidth = 0.5f * thumbWidth;
         thumbHalfHeight = 0.5f * thumbImage.getHeight();
@@ -107,12 +127,13 @@ public class StartPointSeekBar<T extends Number>  extends View
         padding = thumbHalfWidth;
         setFocusable(true);
         setFocusableInTouchMode(true);
-        init();
+        scaledTouchSlop = ViewConfiguration.get(getContext())
+                .getScaledTouchSlop();
     }
 
-    private final void init() {
-        mScaledTouchSlop = ViewConfiguration.get(getContext())
-                .getScaledTouchSlop();
+    public void setAbsoluteMinMaxValue(double absoluteMinValue, double absoluteMaxValue) {
+        this.absoluteMinValue = absoluteMinValue;
+        this.absoluteMaxValue = absoluteMaxValue;
     }
 
     @Override
@@ -130,8 +151,7 @@ public class StartPointSeekBar<T extends Number>  extends View
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event)
-    {
+    public boolean onTouchEvent(MotionEvent event) {
         if (!isEnabled())
             return false;
 
@@ -162,14 +182,14 @@ public class StartPointSeekBar<T extends Number>  extends View
             case MotionEvent.ACTION_MOVE:
                 if (isThumbPressed) {
 
-                    if (mIsDragging) {
+                    if (isDragging) {
                         trackTouchEvent(event);
                     } else {
                         // Scroll to follow the motion event
                         pointerIndex = event.findPointerIndex(mActivePointerId);
                         final float x = event.getX(pointerIndex);
 
-                        if (Math.abs(x - mDownMotionX) > mScaledTouchSlop) {
+                        if (Math.abs(x - mDownMotionX) > scaledTouchSlop) {
                             setPressed(true);
                             invalidate();
                             onStartTrackingTouch();
@@ -184,7 +204,7 @@ public class StartPointSeekBar<T extends Number>  extends View
                 }
                 break;
             case MotionEvent.ACTION_UP:
-                if (mIsDragging) {
+                if (isDragging) {
                     trackTouchEvent(event);
                     onStopTrackingTouch();
                     setPressed(false);
@@ -215,7 +235,7 @@ public class StartPointSeekBar<T extends Number>  extends View
                 invalidate();
                 break;
             case MotionEvent.ACTION_CANCEL:
-                if (mIsDragging) {
+                if (isDragging) {
                     onStopTrackingTouch();
                     setPressed(false);
                 }
@@ -260,8 +280,7 @@ public class StartPointSeekBar<T extends Number>  extends View
     /**
      * Converts screen space x-coordinates into normalized values.
      *
-     * @param screenCoord
-     *            The x-coordinate in screen space to convert.
+     * @param screenCoord The x-coordinate in screen space to convert.
      * @return The normalized value.
      */
     private double screenToNormalized(float screenCoord) {
@@ -276,41 +295,32 @@ public class StartPointSeekBar<T extends Number>  extends View
     }
 
     /**
-            * Converts a normalized value to a Number object in the value space between
-    * absolute minimum and maximum.
-        *
-        * @param normalized
-    * @return
-            */
+     * Converts a normalized value to a Number object in the value space between
+     * absolute minimum and maximum.
+     *
+     * @param normalized
+     * @return
+     */
     @SuppressWarnings("unchecked")
-    private T normalizedToValue(double normalized) {
-        return (T) numberType.toNumber(absoluteMinValuePrim + normalized
-                * (absoluteMaxValuePrim - absoluteMinValuePrim));
+    private double normalizedToValue(double normalized) {
+        return  absoluteMinValue + normalized
+                * (absoluteMaxValue - absoluteMinValue);
     }
 
     /**
      * Converts the given Number value to a normalized double.
      *
-     * @param value
-     *            The Number value to normalize.
+     * @param value The Number value to normalize.
      * @return The normalized double.
      */
-    private double valueToNormalized(T value) {
-        if (0 == absoluteMaxValuePrim - absoluteMinValuePrim) {
-            // prevent division by zero, simply return 0.
-            return 0d;
-        }
-        return (value.doubleValue() - absoluteMinValuePrim)
-                / (absoluteMaxValuePrim - absoluteMinValuePrim);
-    }
 
     private double valueToNormalized(double value) {
-        if (0 == absoluteMaxValuePrim - absoluteMinValuePrim) {
+        if (0 == absoluteMaxValue - absoluteMinValue) {
             // prevent division by zero, simply return 0.
             return 0d;
         }
-        return (value - absoluteMinValuePrim)
-                / (absoluteMaxValuePrim - absoluteMinValuePrim);
+        return (value - absoluteMinValue)
+                / (absoluteMaxValue - absoluteMinValue);
     }
 
 
@@ -318,11 +328,10 @@ public class StartPointSeekBar<T extends Number>  extends View
      * Sets normalized max value to value so that 0 <= normalized min value <=
      * value <= 1. The View will get invalidated when calling this method.
      *
-     * @param value
-     *            The new normalized max value to set.
+     * @param value The new normalized max value to set.
      */
     public void setNormalizedValue(double value) {
-        normalizedThumbValue =  Math.max(0d, value);
+        normalizedThumbValue = Math.max(0d, value);
         invalidate();
     }
 
@@ -330,7 +339,7 @@ public class StartPointSeekBar<T extends Number>  extends View
      * This is called when the user has started touching this widget.
      */
     void onStartTrackingTouch() {
-        mIsDragging = true;
+        isDragging = true;
     }
 
     /**
@@ -338,14 +347,13 @@ public class StartPointSeekBar<T extends Number>  extends View
      * canceled.
      */
     void onStopTrackingTouch() {
-        mIsDragging = false;
+        isDragging = false;
     }
 
     /**
      * Decides which (if any) thumb is touched by the given x-coordinate.
      *
-     * @param touchX
-     *            The x-coordinate of a touch event in screen space.
+     * @param touchX The x-coordinate of a touch event in screen space.
      * @return The pressed thumb or null if none has been touched.
      */
     private boolean evalPressedThumb(float touchX) {
@@ -357,10 +365,8 @@ public class StartPointSeekBar<T extends Number>  extends View
      * Decides if given x-coordinate in screen space needs to be interpreted as
      * "within" the normalized thumb x-coordinate.
      *
-     * @param touchX
-     *            The x-coordinate in screen space to check.
-     * @param normalizedThumbValue
-     *            The normalized x-coordinate of the thumb to check.
+     * @param touchX               The x-coordinate in screen space to check.
+     * @param normalizedThumbValue The normalized x-coordinate of the thumb to check.
      * @return true if x-coordinate is in thumb range, false otherwise.
      */
     private boolean isInThumbRange(float touchX, double normalizedThumbValue) {
@@ -370,8 +376,7 @@ public class StartPointSeekBar<T extends Number>  extends View
     /**
      * Converts a normalized value into screen space.
      *
-     * @param normalizedCoord
-     *            The normalized value to convert.
+     * @param normalizedCoord The normalized value to convert.
      * @return The converted value in screen space.
      */
     private float normalizedToScreen(double normalizedCoord) {
@@ -380,22 +385,19 @@ public class StartPointSeekBar<T extends Number>  extends View
 
 
     @Override
-    protected void onDraw(Canvas canvas)
-    {
+    protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
 
         // draw seek bar background line
         final RectF rect = new RectF(padding,
                 0.5f * (getHeight() - lineHeight), getWidth() - padding,
                 0.5f * (getHeight() + lineHeight));
-        paint.setColor(BACKGROUND_COLOR);
+        paint.setColor(defaultBackgroundColor);
         canvas.drawRect(rect, paint);
 
         // draw seek bar active range line
 
-        if(normalizedToScreen(valueToNormalized(0.0d)) < normalizedToScreen(normalizedThumbValue))
-        {
+        if (normalizedToScreen(valueToNormalized(0.0d)) < normalizedToScreen(normalizedThumbValue)) {
             Log.d(VIEW_LOG_TAG, "thumb: right");
             rect.left = normalizedToScreen(valueToNormalized(0.0d));
             rect.right = normalizedToScreen(normalizedThumbValue);
@@ -405,7 +407,7 @@ public class StartPointSeekBar<T extends Number>  extends View
             rect.left = normalizedToScreen(normalizedThumbValue);
         }
 
-        paint.setColor(SINGLE_COLOR);
+        paint.setColor(defaultRangeColor);
         canvas.drawRect(rect, paint);
 
         drawThumb(normalizedToScreen(normalizedThumbValue),
@@ -417,12 +419,9 @@ public class StartPointSeekBar<T extends Number>  extends View
     /**
      * Draws the "normal" resp. "pressed" thumb image on specified x-coordinate.
      *
-     * @param screenCoord
-     *            The x-coordinate in screen space where to draw the image.
-     * @param pressed
-     *            Is the thumb currently in "pressed" state?
-     * @param canvas
-     *            The canvas to draw upon.
+     * @param screenCoord The x-coordinate in screen space where to draw the image.
+     * @param pressed     Is the thumb currently in "pressed" state?
+     * @param canvas      The canvas to draw upon.
      */
     private void drawThumb(float screenCoord, boolean pressed, Canvas canvas) {
         canvas.drawBitmap(pressed ? thumbPressedImage : thumbImage, screenCoord
@@ -434,7 +433,6 @@ public class StartPointSeekBar<T extends Number>  extends View
      * Utility enumaration used to convert between Numbers and doubles.
      *
      * @author Stephan Tittel (stephan.tittel@kom.tu-darmstadt.de)
-     *
      */
     private static enum NumberType {
         LONG, DOUBLE, INTEGER, FLOAT, SHORT, BYTE, BIG_DECIMAL;
--- startPointSeekBarDemo/src/main/java/com/vashisthg/startpointseekbardemo/MainActivity.java
+++ startPointSeekBarDemo/src/main/java/com/vashisthg/startpointseekbardemo/MainActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
 
 import com.vashisthg.startpointseekbar.StartPointSeekBar;
 
@@ -18,38 +19,12 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        StartPointSeekBar<Integer> seekBar = new StartPointSeekBar<Integer>(-100, +100, this);
-        seekBar.setOnSeekBarChangeListener(new StartPointSeekBar.OnSeekBarChangeListener<Integer>() {
+        StartPointSeekBar seekBar = (StartPointSeekBar) findViewById(R.id.seek_bar);
+        seekBar.setOnSeekBarChangeListener(new StartPointSeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onOnSeekBarValueChange(StartPointSeekBar<?> bar, Integer value) {
+            public void onOnSeekBarValueChange(StartPointSeekBar bar, double value) {
                 Log.d(LOGTAG, "seekbar value:" + value);
             }
         });
-
-        // add RangeSeekBar to pre-defined layout
-        ViewGroup layout = (ViewGroup) findViewById(R.id.seekbarwrapper);
-        layout.addView(seekBar);
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 }
